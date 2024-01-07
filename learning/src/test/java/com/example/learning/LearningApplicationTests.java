84a9d6391cfb01f6e7f8eb90d78129926b2b88ee
JuHeon <75869755+rlawngjs0313@users.noreply.github.com>
2024-01-07 08:55:46 +0000

    init

--- learning/src/test/java/com/example/learning/LearningApplicationTests.java
+++ learning/src/test/java/com/example/learning/LearningApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.spring_learning;
+package com.example.learning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringLearningApplicationTests {
+class LearningApplicationTests {
 
 	@Test
 	void contextLoads() {
